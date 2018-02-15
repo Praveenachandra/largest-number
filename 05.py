9e3a63ee7fc3e5110a0bfa866c08b0014fcd27ef
Praveenachandra <36299134+Praveenachandra@users.noreply.github.com>
2018-02-15 15:23:45 +0530

    Add files via upload

---
+++ 05.py
@@ -0,0 +1,17 @@
+num1= 9
+
+num2= 37
+
+num3= 91
+
+if ((num1>num2) and (num1>num3))
+
+    print ("the largest number is num1")
+
+elif(num2>num3):
+
+    print("the largest number is num2")
+
+else:
+
+    print("the largest number is num3")
